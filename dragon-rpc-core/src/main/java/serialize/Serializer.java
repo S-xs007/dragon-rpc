705bdd7624ccf3438bd7b81dc38ec3c89fd8ca86
coder01 <zhongxuShi_2021@163.com>
2021-07-17 16:06:44 +0800

    序列化

--- dragon-rpc-core/src/main/java/serialize/Serializer.java
+++ dragon-rpc-core/src/main/java/serialize/Serializer.java
@@ -4,6 +4,6 @@ package serialize;
  * 序列化接口
  */
 public interface Serializer {
-    public <T> byte[] serializer(T obj);
+    public <T> byte[] serialize(T obj);
     public <T> Object deserialize(byte[] bytes, Class<T> clazz);
 }
